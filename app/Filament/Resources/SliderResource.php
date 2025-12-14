b07010d32fb6bfc82ce57b61a022d279f7433a09
muhammedfayazp <muhammedfayaz@yahoo.in>
2025-12-14 23:06:12 +0400

    filamentphp

--- app/Filament/Resources/SliderResource.php
+++ app/Filament/Resources/SliderResource.php
@@ -6,84 +6,86 @@ use App\Filament\Resources\SliderResource\Pages;
 use App\Filament\Resources\SliderResource\RelationManagers;
 use App\Models\Slider;
 use Filament\Forms;
-use Filament\Resources\Form;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
 use Filament\Resources\Resource;
-use Filament\Resources\Table;
-use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteBulkAction;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SliderResource extends Resource
 {
     protected static ?string $model = Slider::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    // protected static ?string $navigationIcon = 'rectangle-stack';
+    protected static ?string $navigationGroup = 'Content';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-            Forms\Components\TextInput::make('type')
-                ->required()
-                ->placeholder('homepage, aboutpage, footer, etc.')
-                ->maxLength(255),
+                Forms\Components\TextInput::make('type')
+                    ->required()
+                    ->placeholder('homepage, aboutpage, footer, etc.')
+                    ->maxLength(255),
 
-            Forms\Components\Repeater::make('slides')
-                ->label('Slides')
-                ->schema([
-                    Forms\Components\FileUpload::make('image')
-                        ->image()
-                        ->directory('slides')
-                        ->disk('public')
-                        ->required(),
+                Forms\Components\Repeater::make('slides')
+                    ->label('Slides')
+                    ->schema([
+                        Forms\Components\FileUpload::make('image')
+                            ->image()
+                            ->directory('slides')
+                            ->disk('public')
+                            ->required(),
 
-                    Forms\Components\TextInput::make('title')
-                        ->maxLength(255)
-                        ->nullable(), // title optional
+                        Forms\Components\TextInput::make('title')
+                            ->maxLength(255)
+                            ->nullable(), // title optional
 
-                    Forms\Components\Textarea::make('description')
-                        ->rows(2)
-                        ->nullable(), // description optional
+                        Forms\Components\Textarea::make('description')
+                            ->rows(2)
+                            ->nullable(), // description optional
 
-                    Forms\Components\TextInput::make('link')
-                        ->label('Redirect URL')
-                        ->url() // validate as URL
-                        ->placeholder('https://example.com')
-                        ->nullable(), // link optional
-                ])
-                ->collapsible()
-                ->minItems(1)
-                ->columns(1)
-                ->createItemButtonLabel('Add Slide'),
-        ]);
+                        Forms\Components\TextInput::make('link')
+                            ->label('Redirect URL')
+                            ->url() // validate as URL
+                            ->placeholder('https://example.com')
+                            ->nullable(), // link optional
+                    ])
+                    ->collapsible()
+                    ->minItems(1)
+                    ->columns(1)
+                    ->createItemButtonLabel('Add Slide'),
+            ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('type')->sortable()->searchable(),
-                Tables\Columns\TextColumn::make('updated_at')
+                TextColumn::make('type')->sortable()->searchable(),
+                TextColumn::make('updated_at')
                     ->dateTime('M d, Y H:i'),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
+                DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -91,5 +93,5 @@ class SliderResource extends Resource
             'create' => Pages\CreateSlider::route('/create'),
             'edit' => Pages\EditSlider::route('/{record}/edit'),
         ];
-    }    
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +16,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::firstOrCreate(
+            [
+                'email' => 'admin@example.com',
+            ],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password123'),
+            ]
+        );
     }
 }
